a9a76c021cb22fc247b76577d95953065f929ca0
Ryotaro Abe <45549630+aberyotaro@users.noreply.github.com>
2024-05-02 00:07:32 +0900

    設定見直し

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -21,8 +21,8 @@ return {
   -- These are some examples, uncomment them if you want to see them work!neovim/nvim-lspconf
   {
     "williamboman/mason.nvim",
-    config = function()
-      require("configs.mason").defaults()
+    opts = function()
+      return require "configs.mason"
     end,
   },
   {
@@ -37,6 +37,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = function()
       require "nvchad.configs.treesitter"
+      require "configs.treesitter"
     end,
   },
 
@@ -53,5 +54,19 @@ return {
   {
     "github/copilot.vim",
     lazy = false,
+  },
+  {
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", "gomod" },
+    build = ':lua require("go.install").update_all_sync()', -- if you need to install/update all binaries
   },
 }
