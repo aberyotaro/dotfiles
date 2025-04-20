2b7077658911e0e6452bda41ecd6635d1f442678
aberyotaro <aberyotaro@example.com>
2025-04-20 22:05:49 +0900

    update

--- install.sh
+++ install.sh
@@ -16,13 +16,8 @@ fi
 ln -s "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
 ln -sf "$DOTFILES_DIR/vim/.vimrc"   "$HOME/.vimrc"
 ln -sf "$DOTFILES_DIR/vim/.ideavimrc" "$HOME/.ideavimrc"
-echo "✅ Linked: $HOME/.config/nvim → $DOTFILES_DIR/nvim"
-
-# その他の設定ファイルのシンボリックリンク
-# ln -sf "$DOTFILES_DIR/zsh/.zprofile"          "$HOME/.zprofile"
-# ln -sf "$DOTFILES_DIR/git/.gitconfig"         "$HOME/.gitconfig"
-# ln -sf "$DOTFILES_DIR/git/.gitignore_global"  "$HOME/.gitignore_global"
-# ln -sf "$DOTFILES_DIR/tmux/.tmux.conf"        "$HOME/.tmux.conf"
-# ln -sf "$DOTFILES_DIR/tmux/.tmux.conf.local"  "$HOME/.tmux.conf.local"
+ln -sf ~/dotfiles/tmux/conf ~/.tmux.conf
+ln -sf ~/dotfiles/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -sf ~/dotfiles/nvim ~/.config/nvim
 
 echo "Success"
