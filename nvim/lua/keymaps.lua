6f3fd052250fb8a81bd29bcc8a9cb0317e4b62dd
aberyotaro <aberyotaro@example.com>
2025-04-20 16:52:50 +0900

    fix

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -7,6 +7,7 @@ vim.keymap.set("i", "<C-e>", "<End>", opts)
 vim.keymap.set("i", "<C-h>", "<Left>", opts)
 vim.keymap.set("i", "<C-l>", "<Right>", opts)
 vim.keymap.set("i", "<C-j>", "<Down>", opts)
+vim.keymap.set("i", "<C-k>", "<Nop>", opts)
 vim.keymap.set("i", "<C-k>", "<Up>", opts)
 
 vim.keymap.set("n", "<leader>h", "<C-w>h", opts)
@@ -15,13 +16,13 @@ vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
 vim.keymap.set("n", "<leader>f", ":NvimTreeFindFile<CR>", opts)
 vim.keymap.set("n", "<leader>t", ":NvimTreeFocus<CR>", opts)
 
-# Telescope
+-- Telescope
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
- 
-# Copilot
+
+-- Copilot
 vim.api.nvim_set_keymap('i', '<tab>', 'copilot#Accept()', { expr = true }) -- Copilot で提案を受け入れる
 
